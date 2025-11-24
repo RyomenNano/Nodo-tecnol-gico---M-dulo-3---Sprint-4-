b8e3558b735a80535e3aa5f585a082b190e56dce
fernando <galindez539@gmail.com>
2025-11-24 13:48:38 -0300

    Se añade el formato layout para la vista de la página

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import {connectDB} from './config/DBConfig.mjs';
 import superHeroRoutes from './routes/Rutas.mjs';
 import methodOverride from 'method-override';
+import expressLayouts from 'express-ejs-layouts'
 
 const app=express();
 const PORT=3000;
@@ -13,6 +14,9 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
+app.use(expressLayouts)
+
+app.set('layout', 'layout')
 
 app.use("", superHeroRoutes);
 
